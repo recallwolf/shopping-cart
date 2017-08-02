3be9174b7be92252da61c4453616d96889431103
recallwolf <826628485@qq.com>
2017-08-02 14:07:23 +0800

    beta

---
+++ js/demo.js
@@ -0,0 +1,10 @@
+﻿var data=[
+{"id":101,"name":"可口可乐","thumb_img":"https://img14.360buyimg.com/n0/jfs/t3187/277/6473801213/165201/24ced907/58a57337N7db0db97.jpg","price":"2.50"},
+{"id":102,"name":"雪碧","thumb_img":"https://img14.360buyimg.com/n0/jfs/t3079/17/6384057111/256293/7cf053b5/58a5725cNc30a12af.jpg","price":"2.50"},
+{"id":103,"name":"芬达","thumb_img":"https://img14.360buyimg.com/n0/jfs/t862/201/889109294/88670/94d5b3ea/55593fe0Nf092192e.jpg","price":"2.00"},
+{"id":105,"name":"王老吉","thumb_img":"https://img14.360buyimg.com/n0/jfs/t5749/102/3021254143/209003/7ceacd48/59362d42Ne9c79d3e.jpg","price":"5.00"},
+{"id":107,"name":"百事可乐","thumb_img":"https://img14.360buyimg.com/n0/jfs/t6532/239/1963811069/32426/e268bf1f/595b80efN2cb8e932.jpg","price":"2.50"},
+{"id":108,"name":"加多宝","thumb_img":"https://img14.360buyimg.com/n0/jfs/t3472/222/28766403/459267/bbceaa2/57fc6ae7Nafc10cee.jpg","price":"4.00"},
+{"id":109,"name":"小茗同学","thumb_img":"https://img14.360buyimg.com/n0/jfs/t3106/13/8202798863/56436/9b0304c4/58c11d9aNecf8dcd4.jpg","price":"5.00"},
+{"id":110,"name":"农夫山泉","thumb_img":"https://img14.360buyimg.com/n0/jfs/t2629/320/1572382415/192333/36e90424/5743c996N839ec92e.jpg","price":"2.00"}
+];
